fbf5c207afb4bd646c0dc1b9b0bb07173a9dc7be
Michael Gregoire <mgregoire@nvzion.com>
2023-07-21 08:14:33 -0400

    Add BDS Episode 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,15 +44,15 @@ export default function Home() {
         <h2>Get great music now</h2>
         <nav>
           <BlocLink href="https://blocsonic.com">blocSonic.com</BlocLink>
+          <BlocLink href="https://blocsonic.com/bloccasts/episode/netbloc-originals-headsnack-spotlight">
+            Bloc Discovery Sessions Episode 2: netBloc Originals & Headsnack
+            Spotlight
+          </BlocLink>
           <BlocLink href="https://blocsonic.com/collections/netlabel-day-2023">
             Netlabel Day 2023 Releases
           </BlocLink>
           <BlocLink href="https://blocsonic.com/bloccasts/episode/the-july-2023-episode">
             Tha Bloc Report Episode 59: The July 2023 Episode
-          </BlocLink>
-          <BlocLink href="https://blocsonic.com/bloccasts/episode/whats-up-chuck-origins-and-krazy-yuk">
-            Bloc Discovery Sessions Episode 1: What’s Up Chuck, Origins and
-            Krazy Yuk
           </BlocLink>
           <BlocLink href="https://open.spotify.com/user/blocsonic?si=1cbd5d782966449b">
             Spotify
