9a3df80651e755525e089ca6c5babf4bca682fbb
Michael Gregoire <mgregoire@nvzion.com>
2024-03-08 07:50:11 -0500

    Add TBR 67

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,15 +44,15 @@ export default function Home() {
         <h2>Get great music now</h2>
         <nav>
           <BlocLink href="https://blocsonic.com">blocSonic.com</BlocLink>
+          <BlocLink href="https://blocsonic.com/bloccasts/episode/the-march-2024-episode">
+            Tha Bloc Report Episode 67: The March 2024 Episode
+          </BlocLink>
           <BlocLink href="https://blocsonic.com/releases/tha-silent-partner/capsuletwo-plates-for-nujabes-another-platters-intermission">
             Tha Silent Partner “CAPSULETWO: Plates For Nujabes (Another Platters
             Intermission)”
           </BlocLink>
           <BlocLink href="https://lorenzosmusic.blocsonic.com/">
             Remix Lorenzo’s Music!
-          </BlocLink>
-          <BlocLink href="https://blocsonic.com/bloccasts/episode/the-february-2024-episode">
-            Tha Bloc Report Episode 66: The February 2024 Episode
           </BlocLink>
           <BlocLink href="https://blocsonic.com/bloccasts/episode/netbloc-originals-headsnack-spotlight">
             Bloc Discovery Sessions Episode 2: netBloc Originals & Headsnack
